da84928166dd414906d191d4924233d8f501d314
Rhett Broz <rhettbroz@gmail.com>
2025-06-17 21:34:29 +0000

    add case statement macro

--- models/staging/nyc_taxi/stg_yellow_taxi_trips.sql
+++ models/staging/nyc_taxi/stg_yellow_taxi_trips.sql
@@ -1,7 +1,3 @@
-/*
-taxi trips can be added over time as csv to the main website... think about how to incorporate that
-*/
-
 with 
 
 source as (
@@ -15,39 +11,30 @@ transformed as (
 
 select 
 vendorid as vendor_id
-,case 
-when vendorid = 1 then 'Creative Mobile Technologies, LLC'
-when vendorid = 2 then 'Curb Mobility, LLC'
-when vendorid = 6 then 'Myle Technologies Inc'
-when vendorid = 7 then 'Helix' 
-end as vendor 
+,{{ids_to_values_case(
+    'vendorid'
+    ,[1,2,6,7]
+    ,['Creative Mobile Technologies LLC', 'Curb Mobility, LLC', 'Myle Technologies Inc', 'Helix']
+)}} as vendor
 ,tpep_pickup_datetime as pickup_datetime
 ,tpep_dropoff_datetime as dropoff_datetime
 ,passenger_count
 ,trip_distance as trip_distance_miles
 ,ratecodeid as rate_code_id
-,case 
-when ratecodeid = 1 then 'Standard rate'
-when ratecodeid = 2 then 'JFK'
-when ratecodeid = 3 then 'Newark'
-when ratecodeid = 4 then 'Nassau or Westchester'
-when ratecodeid = 5 then 'Negotiated fare'
-when ratecodeid = 6 then 'Group ride'
-when ratecodeid = 99 then 'Null/unknown'
-end as rate_code 
+,{{ids_to_values_case(
+    'ratecodeid'
+    ,[1,2,3,4,5,6,99]
+    ,['Standard rate', 'JFK', 'Newark', 'Nassau or Westchester','Negotiated fare','Group ride','Null/unknown']
+)}} as rate
 ,store_and_fwd_flag
 ,pulocationid as pickup_location_id 
 ,dolocationid as dropoff_location_id
 ,payment_type as payment_type_id
-,case 
-when payment_type = 0 then 'Flex Fare trip'
-when payment_type = 1 then 'Credit card'
-when payment_type = 2 then 'Cash'
-when payment_type = 3 then 'No charge'
-when payment_type = 4 then 'Dispute'
-when payment_type = 5 then 'Unknown'
-when payment_type = 6 then 'Voided trip'
-end as payment_type
+,{{ids_to_values_case(
+    'payment_type'
+    ,[0,1,2,3,4,5,6]
+    ,['Flex Fare trip', 'Credit card', 'Cash', 'No charge','Dispute','Unknown','Voided trip']
+)}} as payment
 ,round(fare_amount,2) as fare_amount
 ,round(extra,2) as extra
 ,round(mta_tax,2) as mta_tax
@@ -62,6 +49,3 @@ from source
 )
 
 select * from transformed
-
-
-
